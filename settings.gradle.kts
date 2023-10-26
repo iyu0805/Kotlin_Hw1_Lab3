f02d2065531b5c0a965d83341bfd522b4b7ea57b
iyu0805 <t110360142@ntut.org.tw>
2023-10-26 15:46:49 +0800

    1

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin_Hw1_Lab3"
+include(":app")
+ 
